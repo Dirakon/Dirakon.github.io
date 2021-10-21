3a13ad3345da92a9056d857197a8e8b7373adff1
Dirakon <609700zxc@mail.ru>
2021-10-21 21:46:16 +0300

    added project component that is scalable and works fine i think

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -3,7 +3,7 @@ import SingularProject from "./SingularProject";
 
 const ProjectList = function (props) {
     return < div className="projectList">
-        <h1 style={{ textAllign: "center" }}>
+        <h1>
             {props.title}
         </h1>
         {props.projects.map((project, index) =>
--- src/components/SingularProject.jsx
+++ src/components/SingularProject.jsx
@@ -1,10 +1,13 @@
 import React, {useState} from "react";
-
+import image from './../images/airship.jpg'
+import './../styles/App.css'
 const SingularProject = function(props){
-    return < div className = "project" style = {{ backgroundColor : "cyan"}}>
-        <strong>{props.title}</strong>
-        <br/>
-        <a>{props.description}</a>
+    return < div className = "project">
+        <div>
+        <strong className = "projectTitle">{props.title }</strong>
+        <a className = "projectDescription"><br/> {props.description}</a>
+        </div>
+        <img ></img>
     </div>
 }
 
