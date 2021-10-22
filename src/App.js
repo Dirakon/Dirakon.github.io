53d8e07dce86d4e5ab8a782b438bd0d7794f3156
Dirakon <609700zxc@mail.ru>
2021-10-22 20:58:49 +0300

    added colapses with max_height

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './styles/App.css'
 function App() {
   const [projects,setProjects] = useState([
     {title : "Scion", description : "App for programming on mobile devices with my custom visual programming language", image: image},
-    {title : "The Branch Office", description : "Game made in a week about managin workers and not letting the office fall from the branch", image: image},
+    {title : "The Branch Office", description : "Game made in a week about managing workers and not letting the office fall from the branch", image: image},
     {title : "Mobile Continent Generator", description : "Mobile app that lets the user observe some randomly generated worlds and nations", image: image},
     {title : "DialogEditor", description : "Website for creating and experiencing dialogs with my custom dialog language", image: image}
   ]);
--- src/components/SingularProject.jsx
+++ src/components/SingularProject.jsx
@@ -2,15 +2,43 @@ import React, { useState } from "react";
 import image from './../images/airship.jpg'
 import './../styles/App.css'
 const SingularProject = function (props) {
-    
-    return < div className="project" >
-    <img src={props.image} />
-        <div className = "total">
-            <strong className="projectTitle">{props.title}</strong>
-            <br/>
-            <a className="projectDescription">{props.description}</a>
+    let isCurExpanded = false;
+    let clickFunc = function (arg) {
+        arg = arg.target
+        while (!arg.className.startsWith('projectFather')) {
+            arg = arg.parentNode;
+        }
+        arg.classList.toggle("expanded")
+        /*
+        if (isCurExpanded){
+            collapceFunc(arg);
+        }else{
+            expandFunc(arg);
+        }*/
+        isCurExpanded = ! isCurExpanded;
+    }
+
+    let obj = <div className="projectFather">
+        < div className="project" onClick={clickFunc}>
+            <img className="projectImage" src={props.image} />
+            <div className="total">
+                <strong className="projectTitle">{props.title}</strong>
+                <br />
+                <a className="projectDescription">{props.description}</a>
+            </div>
+        </div>
+        <div className="theRealProjectWrapper">
+        <div className="theRealProject">
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </div>
         </div>
     </div>
+
+    return obj
 }
 
 
