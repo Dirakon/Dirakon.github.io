1c863013ea8d566e9f6b937923f4aeaccb854b18
Dirakon <609700zxc@mail.ru>
2023-03-22 17:46:01 +0300

    Fix weird video resolution when not loaded

--- src/components/LongProjectInfo.tsx
+++ src/components/LongProjectInfo.tsx
@@ -1,20 +1,38 @@
-import React, {PropsWithChildren} from "react";
+import React, {PropsWithChildren, useEffect, useRef, useState} from "react";
 import './../styles/LongProjectInfo.css'
 
 const LongProjectInfo = function (props: PropsWithChildren<{ video: string | undefined; }>) {
+    const [videoIsLoaded, setVideoLoadedStatus] = useState(false);
+
+    let videoElement = useRef<HTMLVideoElement>(null);
+    useEffect(() => {
+        let interval = setInterval(() => {
+            if (videoElement != null && videoElement.current != null && videoElement.current.readyState >= 3) {
+                clearInterval(interval);
+                setVideoLoadedStatus(true)
+            }
+        }, 500);
+    }, [])
     return <div className="longProjectInfoWrapper">
         <div className="longProjectInfo">
             {
+                (props.video === undefined || videoIsLoaded) ?
+                    undefined :
+                    <a style={{display: "block", textAlign: "center"}}>...loading video...</a>
+            }
+            {
                 props.video === undefined ?
                     undefined
-                    : <video width="100%" autoPlay loop muted
+                    : <video width={videoIsLoaded ? "100%" : "0%"} autoPlay loop muted ref={videoElement}
                              playsInline>
-                        <source src={props.video} type="video/mp4"/>
+                        <source src={props.video}
+                                type="video/mp4"/>
                     </video>
             }
             <br/>
             {props.children}
         </div>
+        )
     </div>
 }
 
